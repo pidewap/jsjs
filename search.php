2874aba0e3bdcc86cfc2e4aa73d8163b9c32518b
pidewap <pidewap@gmail.com>
2020-11-25 20:31:06 -0800

    Update search.php

--- search.php
+++ search.php
@@ -16,7 +16,7 @@ $json = json_decode($int_data,1);
 
         $k = 0;
         $data = [];
-        print_r($listing);
+        //print_r($listing);
         foreach ($listing as $dataz) {
             if(isset($dataz['compactVideoRenderer']['videoId'])){
                 $duration = $yt->covertime(@$dataz['compactVideoRenderer']['lengthText']['runs'][0]['text']);
