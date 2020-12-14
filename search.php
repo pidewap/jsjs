a8bb0b82cca9e1d59a63a7de98df4e54032b1cfe
pidewap <pidewap@gmail.com>
2020-12-14 00:01:05 -0800

    Update search.php

--- search.php
+++ search.php
@@ -2,33 +2,33 @@
 require('simple_html_dom.php');
 $teks_asli = $_GET['q'];
 $hasil = str_replace([' '], ['-'], $teks_asli);
-$url='https://www.google.co.id/search?q='.$hasil.'-youtube&tbm=vid';
+$url='https://www.google.com/search?q='.$hasil.'-youtube&tbm=vid&tbs=srcf:H4sIAAAAAAAAAC2MQQ6AIBDEfsPFhD8B7mEizBJ20fh7DXpr2qTbrdNnlli0hUNbT7awkOCuFENawgfyJOX6u1rTrhWe-IqIPYAmwyvOb_1YASGYprloAAAA&lr=lang_id';
 // Create DOM from URL or file
 $html = file_get_html($url);
 
-// creating an array of elements
+
 $videos = [];
 
-// Find top ten videos
+
 $i = 1;
 foreach ($html->find('div[class=ZINbbc xpd O9g5cc uUPGi]') as $video) {
         if ($i > 10) {
                 break;
         }
 
-        // Find item link element
+
         $videoDetails = $video->find('div.kCrYT', 0);
 
-        // get title attribute
+
         $videoTitle = str_replace(' - YouTube','', $videoDetails->find('h3', 0)->plaintext);
 
-        // get href attribute
+
         $videoUrl = urldecode($videoDetails->find('a', 0)->href);
         $videUrl = explode("=",$videoUrl);
         $videoUrl = str_replace("&sa", "", $videUrl[2]);
         $videoDatee = $video->find('div[class=BNeawe s3v9rd AP7Wnd]', 0);
         $videoDate = $videoDatee->find('span', 0)->plaintext;
-        // push to a list of videos
+
         $videos[] = [
                 'title' => $videoTitle,
                 'url' => $videoUrl,
@@ -39,4 +39,6 @@ foreach ($html->find('div[class=ZINbbc xpd O9g5cc uUPGi]') as $video) {
 }
 
 echo json_encode($videos, JSON_UNESCAPED_UNICODE);
+
+//echo $html;
 ?>
